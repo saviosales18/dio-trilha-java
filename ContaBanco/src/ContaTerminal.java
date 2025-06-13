f94f9ee87fb0679b85ecbaa8e17186f4a058b07b
Savio Sales <saviosales18@gmail.com>
2025-06-13 13:45:21 -0300

    criando logica

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Por favor, digite o número da conta:");
+            int numeroConta = scanner.nextInt();
+
+            System.out.println("Por favor, digite o número da agência:");
+            String numeroAgencia = scanner.next();
+
+            System.out.println("Por favor, digite o nome do cliente:");
+            String nomeCliente = scanner.next();
+
+            System.out.println("Por favor, digite o saldo da conta:");
+            double saldoConta = scanner.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+            System.out.println("Sua conta é " + numeroConta + " e sua agência é " + numeroAgencia + ".");
+            System.out.printf("Seu saldo é R$ %.2f%n", saldoConta);
+            System.out.println("Aproveite nossos serviços!");
         
+        }
     }
+
 }
